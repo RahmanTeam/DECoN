bd4c4bddd8244ceb89169b316271c957d29018f9
Patrick <patricki.lombard@gosh.nhs.uk>
2020-04-01 14:52:33 +0100

    hopefully this works

--- Linux/ReadInBams.R
+++ Linux/ReadInBams.R
@@ -31,8 +31,94 @@ if(is.null(output)){output="DECoNBams"}
 
 
 #################################
+library(ExomeDepth)
+library(parallel)
+library(foreach)
 
-source('getBamCounts.R')
+
+
+
+myBamCounts <- function(bed.frame = NULL, bed.file = NULL, bam.files, index.files = bam.files,
+                         min.mapq = 20, read.width = 300, include.chr = FALSE, referenceFasta = NULL) {
+
+  if (is.null(bed.frame)) {
+    if (is.null(bed.file)) {
+      stop("If no bed data frame is provided there must be a link to a bed file")
+    }
+    bed.frame <- read.delim(file = bed.file, header =  FALSE, stringsAsFactors = FALSE)
+  }
+
+  names(bed.frame)[1] <- 'seqnames'
+  names(bed.frame)[2] <- 'start'
+  names(bed.frame)[3] <- 'end'
+
+  if (include.chr) {
+    if (sum(grepl(pattern = '^chr', bed.frame$seqnames) > 0)) {
+      warning('The option include.chr == TRUE adds the chr prefix to the chromosome name but it looks like the chromosome names already have a chr prefix. The argument to getBamCounts is probably an error.')
+    }
+    bed.frame$seqnames <- paste('chr', bed.frame$seqnames, sep = '')
+  }
+  
+  chr.names.used <- unique(as.character(bed.frame$seqnames))
+  chr.levels <- c(as.character(seq(1, 22)), subset( chr.names.used, ! chr.names.used %in% as.character(seq(1, 22))))
+
+  bed.frame$seqnames <- factor(bed.frame$seqnames, levels = chr.levels)  ####specifying the levels is important here to not mess up the order
+  bed.frame <- bed.frame[ order(bed.frame$seqnames, bed.frame$start + bed.frame$end), ]  ##order the data frame by position
+
+  target <- GenomicRanges::GRanges(seqnames = bed.frame$seqnames,  
+                    IRanges::IRanges(start=bed.frame$start+1,end=bed.frame$end))
+  
+  rdata <- IRanges::RangedData(space= GenomicRanges::seqnames(target),
+                               ranges=GenomicRanges::ranges(target))
+  
+  if  ((ncol(bed.frame) >= 4) && (class(bed.frame[,4]) %in% c('character', 'factor'))) {    
+    row.names(rdata) <- make.unique(as.character(bed.frame[,4]))  ##add exon names if available
+  }
+  
+############################################################################# add GC content
+if (!is.null(referenceFasta)) {
+  message('Reference fasta file provided so ExomeDepth will compute the GC content in each window')
+    target.dnastringset <- Rsamtools::scanFa(referenceFasta, target)
+  
+    getGCcontent <- function(x) {
+      GC.count <- Biostrings::letterFrequency(x,"GC")
+      all.count <- Biostrings::letterFrequency(x,"ATGC")
+      as.vector(ifelse(all.count==0,NA,GC.count/all.count))
+    }
+    rdata[["GC"]] <- getGCcontent(target.dnastringset)
+  }
+
+############################################################################# Parse BAM files
+  nfiles <- length(bam.files)
+  message('Parse ', nfiles, ' BAM files')
+  print(bam.files)
+
+
+  my.param <- Rsamtools::ScanBamParam(flag = Rsamtools::scanBamFlag(isDuplicate = FALSE, isPaired = TRUE, isProperPair = TRUE, isNotPrimaryRead = FALSE),
+                                      what = c("mapq", "pos", "isize"), )
+
+  # for (i in 1:nfiles) {
+  #   bam <- bam.files[ i ]
+  #   index <- index.files[ i ]
+  #   rdata[[ basename(bam) ]] <- countBamInGRanges.exomeDepth ( bam.file = bam, index = index, granges = target, min.mapq = min.mapq, read.width = read.width)
+  #   message("Number of counted fragments : ", sum(rdata[[ basename(bam) ]]))
+  # }
+  numCores <- detectCores()
+  cl <- parallel::makeForkCluster(numCores)
+  doParallel::registerDoParallel(cl)
+  r <- foreach(i=1:nfiles, .combine='cbind') %dopar% {
+    bam <- bam.files[ i ]
+    index <- index.files[ i ]
+    ExomeDepth::countBamInGRanges.exomeDepth ( bam.file = bam, index = index, granges = target, min.mapq = min.mapq, read.width = read.width)
+  }
+  parallel::stopCluster(cl)
+  colnames(r) <- basename(bam.files)
+  rdata <- data.frame(rdata)
+  rdata <- cbind(rdata, r)
+
+  return(data.frame(rdata))
+}
+
 
 multi_strsplit<-function(x,splits,y){                                                   #function which recursively splits x by an element of 'splits' then extracts the y element of the split vector
 	X<-x
@@ -62,7 +148,7 @@ names(sample.names)<-NULL
 bed.file<-read.table(paste(bedfile))                                                    #reads in the bedfile and gives each column a name - expects 4 columns: chr, start, stop, name/gene.
 colnames(bed.file)<-c("chromosone","start","end","name")
  
-counts <- getBamCounts(bed.frame = bed.file, bam.files = bams, include.chr = FALSE, referenceFasta = paste(fasta))
+counts <- myBamCounts(bed.frame = bed.file, bam.files = bams, include.chr = FALSE, referenceFasta = paste(fasta))
                                                                                         #reads in coverage info from each bam file in 'bams'; expects chromosomes to be given as numbers, eg 1, 2 etc, not chr1, chr2 etc.
 save(counts,bams,bed.file,sample.names,fasta,file=paste(output,".RData",sep=""))                                   #saves workspace as 'output_counts.RData'
    
