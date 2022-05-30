1861f3224163200ff6b3a4f76779fcac636c8ab2
AnnaFowler <a.fowler@liv.ac.uk>
2022-05-30 15:02:53 +0100

    Updating to use renv instead of packrat

--- Linux/.Rprofile
+++
@@ -1,4 +0,0 @@
-#### -- Packrat Autoloader (version 0.4.3) -- ####
-options(repos=structure(c(CRAN="http://mirrors.ebi.ac.uk/CRAN/")))
-source("packrat/init.R")
-#### -- End Packrat Autoloader -- ####
--- Linux/setup.sh
+++ Linux/setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-[ -w ".Rprofile" ] && rm .Rprofile
 
-Rscript sessionInfo.R --bootstrap-packrat > setup.log 2>&1
+Rscript sessionInfo.R > setup.log 2>&1
 
-cp packrat/packrat_source/.Rprofile ./
